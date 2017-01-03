96f443f8c35687cc4d59c2856b7cd7815b3a3c79
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2017-01-03 20:37:17 +0100

    Compile synthetic.siemens-i-ras

--- data/synthetic/versions/siemens-i-ras/compile/src/Encrypting.java
+++ data/synthetic/versions/siemens-i-ras/compile/src/Encrypting.java
@@ -2,7 +2,7 @@ import javax.crypto.spec.SecretKeySpec;
 import javax.crypto.Cipher;
 
 public class Encrypting {
-	public static byte[] encryptWithKey(byte[] content) {
+	public static byte[] encryptWithKey(byte[] content) throws Exception {
 		// using a constant key is unsafe
 		SecretKeySpec keySpec = new SecretKeySpec("RAS".getBytes("UTF-8"), "AES/CBC/PKCS5Padding");
 		Cipher c = Cipher.getInstance("AES");
@@ -10,7 +10,7 @@ public class Encrypting {
 		return c.doFinal(content);
 	}
 	
-	private static byte[] encrypt(byte[] key, byte[] content) {
+	private static byte[] encrypt(byte[] key, byte[] content) throws Exception {
 		// "AES" has an unsafe default configuration of "AES/EBC/PKCS5Padding"
 		SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
 		Cipher c = Cipher.getInstance("AES");
